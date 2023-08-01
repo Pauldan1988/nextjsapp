478ba956faf09f55283ab846935ff89b28ed3fe2
Pal Paypal <pdangelo1988@gmail.com>
2023-07-31 22:11:01 -0400

    working on delete route

--- src/app/api/delete/[id]/route.ts
+++ src/app/api/delete/[id]/route.ts
@@ -1,18 +1,12 @@
 import { prisma } from "@/db"
 import { NextResponse } from "next/server"
+
 //@ts-ignore
 export async function DELETE(req: Request, context: any) {
+    const 
     
-    const { id } = context.params
-    console.log(req)
-    try {
+    const delTask = await prisma.todo.delete()
+ 
 
-        await prisma.todo.delete({
-            where: { id }
-        })
-        return NextResponse.json({ message: "Todo deleted successfully" })
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ message: "Something went wrong" })
-    }
+    return NextResponse.json(`/api/get`)
 }
