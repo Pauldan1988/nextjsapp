84d5293b828a606f4a1046cfd369e8681fd10c28
Paul Dangelo <pdangelo1988@gmail.com>
2023-08-10 14:59:20 -0400

    Started onChange input for save button

--- src/components/Todoitem.tsx
+++ src/components/Todoitem.tsx
@@ -7,122 +7,123 @@ import { TasksDispatchContext } from "./TasksContext";
 //*useState is a react hook that allows you to use state in a functional component
 
 type TodoItemProps = {
-    task: {
-        id: string;
-        title: string;
-        complete: boolean;
-        isEditing: boolean;
-    };
+  task: {
+    id: string;
+    title: string;
+    complete: boolean;
+    isEditing: boolean;
+  };
 };
 //*What my todoitem components looks like when I call it in other functions
 //*onChange event listener is used to toggle the todo item and pass an id to the database
 
 export default function TodoItem({ task }: TodoItemProps) {
-    const [edit, setEdit] = useState(false);
-    const [showDelete, setShowDelete] = useState(false);
-    const [newTitle, setNewTitle] = useState("");
-    const dispatch = useContext(TasksDispatchContext);
+  const [edit, setEdit] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
+  const dispatch = useContext(TasksDispatchContext);
 
-    function handleChange(e: React.FormEvent) {
-        e.preventDefault()
-        //@ts-ignore
-        const title = e.target.title.value
-        //@ts-ignore
-        const id = e.target.id
-        //@ts-ignore
-        dispatch({
-            type: "edited",
+  function handleChange(e: React.FormEvent) {
+    e.preventDefault()
+    //@ts-ignore
+    const title = e.target.title.value
+    //@ts-ignore
+    const id = e.target.id
+    //@ts-ignore
+    dispatch({
+      type: "edited",
+      task: {
+        ...task
+      }
+    })
+  }
+
+  return (
+    <li
+      onMouseEnter={() => setShowDelete(true)}
+      onMouseLeave={() => setShowDelete(false)}
+      className="flex gap-1 items-center"
+    >
+      <input
+        id={task.id}
+        type="checkbox"
+        className="cursor-pointer peer"
+        checked={task.complete}
+        onChange={(e) =>
+          //@ts-ignore
+          dispatch({
+            type: "changed",
             task: {
-                text: title,
-                id: id,
+              ...task,
+              complete: e.target.checked,
+            },
+          })
+        }
+      />
+      <label
+        htmlFor={task.id}
+        className="group relative cursor-pointer peer-checked:line-through"
+      >
+        {task.title}
+      </label>
+
+      {showDelete ? (
+        <div className="group relative focus: text-slate-400 hover:text-blue-500">
+          <button
+            onClick={() =>
+              //@ts-ignore
+              dispatch({
+                type: "edited",
+                task: {
+                  ...task,
+                },
+              })
             }
-        })
-    }
+          >
+            Edit
+          </button>
+        </div>
+      ) : null}
 
-    return (
-        <li
-            onMouseEnter={() => setShowDelete(true)}
-            onMouseLeave={() => setShowDelete(false)}
-            className="flex gap-1 items-center"
+      <form onSubmit={handleChange}>
+        <input
+
+          onChange={(e) => setNewTitle(e.target.value)}
+          className="cursor-pointer peer"
+
+        />
+      </form>
+      <div className="group relative focus: text-slate-400 hover:text-blue-500">
+        <button
+          onClick={() =>
+            //@ts-ignore
+            dispatch({
+              type: "saved",
+              title: task.title,
+              id: task.id,
+            })
+          }
         >
-            <input
-                id={task.id}
-                type="checkbox"
-                className="cursor-pointer peer"
-                checked={task.complete}
-                onChange={(e) =>
-                    //@ts-ignore
-                    dispatch({
-                        type: "changed",
-                        task: {
-                            ...task,
-                            complete: e.target.checked,
-                        },
-                    })
-                }
-            />
-            <form onSubmit={handleChange}>
-                <input
-                    id={task.id}
-                    type="text"
-                    className="cursor-pointer peer"
-                />
-            </form>
-            <label
-                htmlFor={task.id}
-                className="group relative cursor-pointer peer-checked:line-through"
-            >
-                {task.title}
-            </label>
-
-            {showDelete ? (
-                <div className="group relative focus: text-slate-400 hover:text-blue-500">
-                    <button
-                        onClick={() =>
-                            //@ts-ignore
-                            dispatch({
-                                type: "edited",
-                                task: {
-                                    ...task,
-                                },
-                            })
-                        }
-                    >
-                        Edit
-                    </button>
-                </div>
-            ) : null}
-                <div className="group relative focus: text-slate-400 hover:text-blue-500">
-                    <button
-                        onClick={() =>
-                            //@ts-ignore
-                            dispatch({
-                                type: "saved",
-                                title: task.title,
-                                id: task.id,
-                            })
-                        }
-                    >
-                        Save
-                        </button>
-                    </div>
-            {showDelete ? (
-                <div className="group relative focus: text-slate-400 hover:text-red-500">
-                    <button
-                        onClick={() =>
-                            //@ts-ignore
-                            dispatch({
-                                type: "deleted",
-                                id: task.id,
-                            })
-                        }
-                    >
-                        Delete
-                    </button>
-                </div>
-            ) : null}
-        </li>
-    );
+          Save
+        </button>
+      </div>
+      {showDelete ? (
+        <div className="group relative focus: text-slate-400 hover:text-red-500">
+          <button
+            onClick={() =>
+              //@ts-ignore
+              dispatch({
+                type: "deleted",
+                id: task.id,
+              })
+            }
+          >
+            Delete
+          </button>
+        </div>
+      ) : null}
+    </li>
+  );
 }
 
 //* if edit = true, render save button(and input)
@@ -134,7 +135,7 @@ export default function TodoItem({ task }: TodoItemProps) {
 
 //// -Save button(otherwise I will have to send a PATCH request on each changeEvent)
 
-// -New state object called newTitle
+//// -New state object called newTitle
 
 // -Create handle change function(Takes in FormEvent, Sets newTitle State). Goes in the new input field on Todoitem.tsx
 
