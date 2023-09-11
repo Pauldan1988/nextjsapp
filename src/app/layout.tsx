5cd5253c456751ad90c041a55f3651187bc92a8c
Paul Dangelo <pdangelo1988@gmail.com>
2023-09-10 20:09:33 -0400

    Webpack Errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import './globals.css'
+import { Inter } from 'next/font/google'
 import { TasksContext, TasksDispatchContext, tasksReducer } from '../components/TasksContext'
 import { useReducer, useEffect, useState } from 'react'
 import axios from 'axios'
 
+
+const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({
   children,
 }: {
@@ -25,7 +28,6 @@ useEffect( () => {
     []
   );
   return (
-
     <html lang="en">
       <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}>
         <TasksContext.Provider value={tasks}>
