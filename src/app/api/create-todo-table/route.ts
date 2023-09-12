71557a56dbf9e283b83ff22189a48f50aa140ed0
Paul Dangelo <pdangelo1988@gmail.com>
2023-09-11 23:16:39 -0400

    added vercel postgres API route

--- src/app/api/create-todo-table/route.ts
+++ src/app/api/create-todo-table/route.ts
@@ -1,12 +1,13 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
-export async function GET(request: Request) {
+export async function GET(req: Request) {
   try {
     const result =
-      await sql`CREATE TABLE Todo ( Name varchar(255), Owner varchar(255) );`;
+      await sql`SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'todo')`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
+    console.log(error)
     return NextResponse.json({ error }, { status: 500 });
   }
 }
