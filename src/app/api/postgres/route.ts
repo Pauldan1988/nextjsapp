8f5a265afcd03da0536f1c80ae84e34918261734
Paul Dangelo <pdangelo1988@gmail.com>
2023-09-12 23:56:07 -0400

    Finishing connecting application to vercel postgres serverless database. ALMOST DONE

---
+++ src/app/api/postgres/route.ts
@@ -0,0 +1,13 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(req: Request) {
+  try {
+    const result =
+      await sql`SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'todo')`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
